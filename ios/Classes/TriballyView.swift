5ca79957c700aa1352f788a775df63c25e9f037d
Roman <225000@niuitmo.ru>
2022-04-08 12:15:12 +0300

    format

--- ios/Classes/TriballyView.swift
+++ ios/Classes/TriballyView.swift
@@ -4,68 +4,68 @@ import Tribally
 
 public class TriballyView: NSObject, FlutterPlatformView, UINavigationControllerDelegate
 {
-private let triballyView: TriballyViewController
-public let methodChannel: FlutterMethodChannel!
-  public let pluginRegistrar: FlutterPluginRegistrar!
-
-public required init(id: Int64, frame: CGRect, registrar: FlutterPluginRegistrar, params: [String: Any]) {
-
-    self.pluginRegistrar = registrar
-
-    var settings = params["settings"] as! [String: Any]
-    var viewOptions = params["viewOptions"] as! [String: Any]
-
-    var avatarUrl: URL?
-          if let avatar = settings["avatar"] as? String {
+    private let triballyView: TriballyViewController
+    public let methodChannel: FlutterMethodChannel!
+    public let pluginRegistrar: FlutterPluginRegistrar!
+    
+    public required init(id: Int64, frame: CGRect, registrar: FlutterPluginRegistrar, params: [String: Any]) {
+        
+        self.pluginRegistrar = registrar
+        
+        var settings = params["settings"] as! [String: Any]
+        var viewOptions = params["viewOptions"] as! [String: Any]
+        
+        var avatarUrl: URL?
+        if let avatar = settings["avatar"] as? String {
             avatarUrl = .init(string: avatar)
-          }
-    self.triballyView = TriballyViewController(
-                                                               configuration: .init(
-                                                                  projectId: settings["projectId"] as! String,
-                                                                  apiKey: settings["apiKey"] as! String,
-                                                                  externalId: settings["uid"] as! String,
-                                                                  displayName: settings["displayName"] as! String,
-                                                                  avatar: avatarUrl
-                                                                  ),
-                                                                  themeProvider: .init(
-                                                                    primary: UIColor(hexString: viewOptions["primary"] as! String),
-                                                                      background: UIColor(hexString: viewOptions["background"] as! String),
-                                                                      surface: UIColor(hexString: viewOptions["surface"] as! String),
-                                                                      text: UIColor(hexString: viewOptions["text"] as! String),
-                                                                      primaryDark: UIColor(hexString: viewOptions["primaryDark"] as! String),
-                                                                      subText: UIColor(hexString: viewOptions["subText"] as! String),
-                                                                      icon: UIColor(hexString: viewOptions["icon"] as! String)
-                                                                  ))
-    self.triballyView.view.frame = frame
+        }
+        self.triballyView = TriballyViewController(
+            configuration: .init(
+                projectId: settings["projectId"] as! String,
+                apiKey: settings["apiKey"] as! String,
+                externalId: settings["uid"] as! String,
+                displayName: settings["displayName"] as! String,
+                avatar: avatarUrl
+            ),
+            themeProvider: .init(
+                primary: UIColor(hexString: viewOptions["primary"] as! String),
+                background: UIColor(hexString: viewOptions["background"] as! String),
+                surface: UIColor(hexString: viewOptions["surface"] as! String),
+                text: UIColor(hexString: viewOptions["text"] as! String),
+                primaryDark: UIColor(hexString: viewOptions["primaryDark"] as! String),
+                subText: UIColor(hexString: viewOptions["subText"] as! String),
+                icon: UIColor(hexString: viewOptions["icon"] as! String)
+            ))
+        self.triballyView.view.frame = frame
+        
+        self.methodChannel = FlutterMethodChannel(
+            name: "tribally/tribally_veiw_\(id)",
+            binaryMessenger: registrar.messenger()
+        )
+        
+        super.init()
+        self.triballyView.delegate = self
+        weak var weakSelf = self
+        self.methodChannel.setMethodCallHandler({ weakSelf?.handle($0, result: $1) })
+        
+    }
     
-    self.methodChannel = FlutterMethodChannel(
-      name: "tribally/tribally_veiw_\(id)",
-      binaryMessenger: registrar.messenger()
-    )
-
-    super.init()
-    self.triballyView.delegate = self
-    weak var weakSelf = self
-    self.methodChannel.setMethodCallHandler({ weakSelf?.handle($0, result: $1) })
-
-  }
     
-
-  public func view() -> UIView {
-      return self.triballyView.view
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-  let params = call.arguments as! [String: Any]
-switch call.method {
-    case "setPushToken":
-      triballyView.setPushToken(params["deviceToken"] as! String)
-      default:
+    public func view() -> UIView {
+        return self.triballyView.view
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let params = call.arguments as! [String: Any]
+        switch call.method {
+        case "setPushToken":
+            triballyView.setPushToken(params["deviceToken"] as! String)
+        default:
             result(FlutterMethodNotImplemented)
-
-      }
-  }
-
+            
+        }
+    }
+    
 }
 
 extension UIColor {
